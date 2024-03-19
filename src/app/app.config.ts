5e3a2a9ee8576e2b93444a4c7ba8000f44eac045
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2024-03-19 13:23:35 +0100

    TT-23: Remove the unnecessary provider for client hydration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,9 @@
 import { ApplicationConfig } from "@angular/core";
-import { provideClientHydration } from "@angular/platform-browser";
 import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
 import { provideRouter } from "@angular/router";
 
 import { routes } from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
-    providers: [
-        provideRouter(routes),
-        provideClientHydration(),
-        provideAnimationsAsync(),
-    ],
+    providers: [provideRouter(routes), provideAnimationsAsync()],
 };
