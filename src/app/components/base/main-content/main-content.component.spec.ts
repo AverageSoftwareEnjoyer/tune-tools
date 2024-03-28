e194af8e7724999cab74d62c0a7b97e222c6d8d2
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2024-03-28 21:26:08 +0100

    fixup! WIP on TT-23-base-components: e552234 fixup! TT-23: Update Playwright config and add e2e tests

--- src/app/components/base/main-content/main-content.component.spec.ts
+++ src/app/components/base/main-content/main-content.component.spec.ts
@@ -1,23 +1,52 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { MatSidenav } from "@angular/material/sidenav";
 import { By } from "@angular/platform-browser";
+import { Observable, Subject } from "rxjs";
 
+import { SidenavService } from "../sidenav.service";
 import { MainContentComponent } from "./main-content.component";
 
 describe("MainContentComponent", () => {
     let component: MainContentComponent;
     let fixture: ComponentFixture<MainContentComponent>;
+    let toggleSidenavSubject: Subject<void>;
+    let mockSidenavService: {
+        toggleSidenav$: Observable<void>;
+    };
+    let mockSidenav: Partial<MatSidenav>;
 
     beforeEach(() => {
+        mockSidenav = { toggle: jest.fn() };
+
+        toggleSidenavSubject = new Subject<void>();
+        mockSidenavService = {
+            toggleSidenav$: toggleSidenavSubject.asObservable(),
+        };
+
         TestBed.configureTestingModule({
             imports: [MainContentComponent],
+            providers: [
+                { provide: SidenavService, useValue: mockSidenavService },
+            ],
         });
 
         fixture = TestBed.createComponent(MainContentComponent);
         component = fixture.componentInstance;
+
+        component.sidenav = mockSidenav as MatSidenav;
+        fixture.detectChanges();
     });
 
     it("should create", () => {
         expect(component).toBeTruthy();
+    });
+
+    it("should toggle sidenav on sidenavService event", () => {
+        toggleSidenavSubject.subscribe(() => {
+            expect(mockSidenav.toggle).toHaveBeenCalled();
+        });
+
+        toggleSidenavSubject.next();
     });
 
     it("should contain a router outlet", () => {
