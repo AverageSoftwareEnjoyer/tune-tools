829373c2d7150054a489be0777fa845c9f81d2fa
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2024-04-06 22:28:08 +0200

    TT-28: Clean up existing code and use the destroy class

--- src/app/components/base/main-content/main-content.component.ts
+++ src/app/components/base/main-content/main-content.component.ts
@@ -6,15 +6,14 @@ import {
     Component,
     inject,
     Input,
-    OnDestroy,
     ViewChild,
 } from "@angular/core";
 import { MatIconModule } from "@angular/material/icon";
 import { MatListModule } from "@angular/material/list";
 import { MatSidenav, MatSidenavModule } from "@angular/material/sidenav";
 import { RouterModule } from "@angular/router";
-import { Subscription } from "rxjs";
 
+import { DestroyClass } from "../../../core/destroy/destroy.class";
 import { HeaderComponent } from "../header/header.component";
 import { SidenavService } from "../sidenav.service";
 
@@ -33,24 +32,21 @@ import { SidenavService } from "../sidenav.service";
     styleUrls: ["./main-content.component.scss"],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MainContentComponent implements AfterViewInit, OnDestroy {
+export class MainContentComponent
+    extends DestroyClass
+    implements AfterViewInit {
     @ViewChild("menu") sidenav!: MatSidenav;
     @Input() isHandset!: boolean | null;
-
-    #toggleSubscription = new Subscription();
 
     readonly #sidenavService = inject(SidenavService);
     readonly #changeDetectorRef = inject(ChangeDetectorRef);
 
     ngAfterViewInit(): void {
-        this.#toggleSubscription =
-            this.#sidenavService.toggleSidenav$.subscribe(() => {
+        this.#sidenavService.toggleSidenav$
+            .pipe(this.untilDestroyed())
+            .subscribe(() => {
                 this.sidenav.toggle();
                 this.#changeDetectorRef.markForCheck();
             });
-    }
-
-    ngOnDestroy(): void {
-        this.#toggleSubscription.unsubscribe();
     }
 }
