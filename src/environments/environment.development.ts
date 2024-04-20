73705b8ea5b5a07c993c0c3f1bb8425421ff70e0
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2024-04-20 19:11:31 +0200

    TT-34: Create environment files

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+import { Environment } from "./environment.model";
+
+export const environment: Environment = {
+    production: false,
+    clientId: "d9716dd3efb4408491738456849796a1",
+    redirectUri: "http://localhost:4200",
+    scope: "playlist-modify-private playlist-modify-public user-read-private user-read-recently-played user-top-read",
+    authUrl: "https://accounts.spotify.com/authorize",
+    tokenUrl: "https://accounts.spotify.com/api/token",
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+import { Environment } from "./environment.model";
+
+export const environment: Environment = {
+    production: true,
+    clientId: "d9716dd3efb4408491738456849796a1",
+    redirectUri: "https://averagesoftwareenjoyer.github.io/tune-tools",
+    scope: "playlist-modify-private playlist-modify-public user-read-private user-read-recently-played user-top-read",
+    authUrl: "https://accounts.spotify.com/authorize",
+    tokenUrl: "https://accounts.spotify.com/api/token",
+};
