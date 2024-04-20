24b345dfbb1fc293d9e8d8c74deeadc0c2fd2169
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2024-04-20 17:50:19 +0200

    TT-34: Set up Sentry

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,15 @@
-import { ApplicationConfig } from "@angular/core";
+import {
+    APP_INITIALIZER,
+    ApplicationConfig,
+    ErrorHandler,
+} from "@angular/core";
 import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
 import {
     provideRouter,
     withComponentInputBinding,
     withInMemoryScrolling,
 } from "@angular/router";
+import * as Sentry from "@sentry/angular-ivy";
 
 import { routes } from "./app.routes";
 
@@ -19,5 +24,18 @@ export const appConfig: ApplicationConfig = {
             }),
         ),
         provideAnimationsAsync(),
+        {
+            provide: ErrorHandler,
+            useValue: Sentry.createErrorHandler({
+                showDialog: false,
+                logErrors: true,
+            }),
+        },
+        {
+            provide: APP_INITIALIZER,
+            useFactory: () => () => ({}),
+            deps: [Sentry.TraceService],
+            multi: true,
+        },
     ],
 };
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,23 @@
 import { bootstrapApplication } from "@angular/platform-browser";
+import * as Sentry from "@sentry/angular-ivy";
 
 import { AppComponent } from "./app/app.component";
 import { appConfig } from "./app/app.config";
+
+Sentry.init({
+    dsn: "https://4a402fb2665571681af69dde2aa3a064@o4506809213714432.ingest.us.sentry.io/4506809217449984",
+    integrations: [
+        Sentry.browserTracingIntegration(),
+        Sentry.replayIntegration(),
+    ],
+    tracesSampleRate: 1.0,
+    tracePropagationTargets: [
+        "localhost",
+        /^https:\/\/averagesoftwareenjoyer\.github\.io\/tune-tools/,
+    ],
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+});
 
 bootstrapApplication(AppComponent, appConfig).catch((err) => {
     console.error(err);
