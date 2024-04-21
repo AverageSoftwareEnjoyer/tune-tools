919efa267e4c00f53b0fb623ba33aea8dcdcdd09
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2024-04-21 23:51:24 +0200

    fixup! TT-34: Remove throwing an error to test Sentry integration

--- src/main.ts
+++ src/main.ts
@@ -3,21 +3,24 @@ import * as Sentry from "@sentry/angular-ivy";
 
 import { AppComponent } from "./app/app.component";
 import { appConfig } from "./app/app.config";
+import { environment } from "./environments/environment";
 
-Sentry.init({
-    dsn: "https://4a402fb2665571681af69dde2aa3a064@o4506809213714432.ingest.us.sentry.io/4506809217449984",
-    integrations: [
-        Sentry.browserTracingIntegration(),
-        Sentry.replayIntegration(),
-    ],
-    tracesSampleRate: 1.0,
-    tracePropagationTargets: [
-        "localhost",
-        /^https:\/\/averagesoftwareenjoyer\.github\.io\/tune-tools/,
-    ],
-    replaysSessionSampleRate: 0.1,
-    replaysOnErrorSampleRate: 1.0,
-});
+if (environment.production) {
+    Sentry.init({
+        dsn: "https://4a402fb2665571681af69dde2aa3a064@o4506809213714432.ingest.us.sentry.io/4506809217449984",
+        integrations: [
+            Sentry.browserTracingIntegration(),
+            Sentry.replayIntegration(),
+        ],
+        tracesSampleRate: 1.0,
+        tracePropagationTargets: [
+            "localhost",
+            /^https:\/\/averagesoftwareenjoyer\.github\.io\/tune-tools/,
+        ],
+        replaysSessionSampleRate: 0.1,
+        replaysOnErrorSampleRate: 1.0,
+    });
+}
 
 bootstrapApplication(AppComponent, appConfig).catch((err) => {
     console.error(err);
