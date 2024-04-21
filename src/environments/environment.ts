8c7c377cbb4a7b272aac52609f9b3a63895b55e5
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2024-04-21 23:49:32 +0200

    fixup! TT-34: Create environment files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@ import { Environment } from "./environment.model";
 export const environment: Environment = {
     production: true,
     clientId: "d9716dd3efb4408491738456849796a1",
-    redirectUri: "https://averagesoftwareenjoyer.github.io/tune-tools",
+    redirectUri:
+        "https://averagesoftwareenjoyer.github.io/tune-tools/auth-callback",
     scope: "playlist-modify-private playlist-modify-public user-read-private user-read-recently-played user-top-read",
     authUrl: "https://accounts.spotify.com/authorize",
     tokenUrl: "https://accounts.spotify.com/api/token",
