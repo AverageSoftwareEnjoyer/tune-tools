81bdbc6f05db8bf7fd0cb22bdd3d4264c4b45807
Domin8668 <github-actions[bot] 41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 21:28:49 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/AverageSoftwareEnjoyer/tune-tools/commit/3fb398d7fdef0ba192e47583fa8cf82df1cd2db6

---
+++ chunk-6B5GWKLQ.js
@@ -0,0 +1,2 @@
+var t={production:!0,clientId:"d9716dd3efb4408491738456849796a1",redirectUri:"https://averagesoftwareenjoyer.github.io/tune-tools/auth-callback",scope:"playlist-modify-private playlist-modify-public user-read-private user-read-email user-read-recently-played user-top-read",authUrl:"https://accounts.spotify.com/authorize",tokenUrl:"https://accounts.spotify.com/api/token",apiBaseUrl:"https://api.spotify.com/v1"};export{t as a};
+//# sourceMappingURL=chunk-6B5GWKLQ.js.map
