a05e6bce54940733c5608da72df3b0e487c992ce
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2024-06-07 20:45:58 +0200

    TT-42: Replace the deprecated HttpClientTestingModule imports

--- src/app/api/auth-http.service.spec.ts
+++ src/app/api/auth-http.service.spec.ts
@@ -1,4 +1,5 @@
-import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { provideHttpClient } from "@angular/common/http";
+import { provideHttpClientTesting } from "@angular/common/http/testing";
 import { TestBed } from "@angular/core/testing";
 
 import { AuthHTTPService } from "./auth-http.service";
@@ -8,7 +9,7 @@ describe("AuthHTTPService", () => {
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            imports: [HttpClientTestingModule],
+            providers: [provideHttpClient(), provideHttpClientTesting()],
         });
         service = TestBed.inject(AuthHTTPService);
     });
