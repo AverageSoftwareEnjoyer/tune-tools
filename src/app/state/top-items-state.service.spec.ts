1bb6798087ce8f8692384bc8437344f5e1f9435f
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2024-06-23 18:28:15 +0200

    TT-54: Create a service for top items state

---
+++ src/app/state/top-items-state.service.spec.ts
@@ -0,0 +1,106 @@
+import { provideHttpClient } from "@angular/common/http";
+import {
+    HttpTestingController,
+    provideHttpClientTesting,
+} from "@angular/common/http/testing";
+import { fakeAsync, TestBed, tick } from "@angular/core/testing";
+import { TopItemsHTTPService } from "@api/top-items-http.service";
+import { environment } from "@env/environment";
+import {
+    mockTopTrackLimited,
+    mockTopTracks,
+} from "@mocks/top-items.model.mock";
+import { TimeRangeOptions } from "@model/top-items.model";
+import { of } from "rxjs";
+
+import { TopItemsStateService } from "./top-items-state.service";
+
+describe("TopItemsStateService", () => {
+    let topItemsStateService: TopItemsStateService;
+    let topItemsHTTPService: TopItemsHTTPService;
+    let httpTestingController: HttpTestingController;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [provideHttpClient(), provideHttpClientTesting()],
+        });
+        topItemsStateService = TestBed.inject(TopItemsStateService);
+        httpTestingController = TestBed.inject(HttpTestingController);
+        topItemsHTTPService = TestBed.inject(TopItemsHTTPService);
+    });
+
+    afterEach(() => {
+        httpTestingController.verify();
+    });
+
+    it("should be created", () => {
+        expect(topItemsStateService).toBeTruthy();
+    });
+
+    it("should publish a new time range and update the state", () => {
+        const timeRange = TimeRangeOptions.MediumTerm;
+        topItemsHTTPService.getTopItems$ = jest
+            .fn()
+            .mockReturnValue(of(mockTopTracks));
+
+        topItemsStateService.publishTopTracksTimeRange(timeRange);
+
+        expect(topItemsStateService.topTracksTimeRange()).toBe(timeRange);
+    });
+
+    it("should fetch and update top tracks for the specified time range", fakeAsync(() => {
+        const timeRange = TimeRangeOptions.LongTerm;
+        topItemsHTTPService.getTopItems$ = jest
+            .fn()
+            .mockReturnValue(of(mockTopTracks));
+
+        topItemsStateService.publishTopTracksTimeRange(timeRange);
+        tick();
+
+        expect(topItemsStateService.topTracks()).toEqual([mockTopTrackLimited]);
+        expect(topItemsHTTPService.getTopItems$).toHaveBeenCalledTimes(1);
+    }));
+
+    it("should fetch data again if data for the specified time range has not been loaded yed", fakeAsync(() => {
+        jest.spyOn(topItemsHTTPService, "getTopItems$");
+
+        topItemsStateService.publishTopTracksTimeRange(
+            TimeRangeOptions.LongTerm,
+        );
+        const req1 = httpTestingController.expectOne(
+            `${environment.apiBaseUrl}/me/top/tracks?time_range=long_term&limit=50&offset=0`,
+        );
+        req1.flush(mockTopTracks);
+        tick();
+        expect(req1.request.method).toBe("GET");
+        expect(topItemsStateService.topTracks()).toEqual([mockTopTrackLimited]);
+
+        topItemsStateService.publishTopTracksTimeRange(
+            TimeRangeOptions.MediumTerm,
+        );
+        const req2 = httpTestingController.expectOne(
+            `${environment.apiBaseUrl}/me/top/tracks?time_range=medium_term&limit=50&offset=0`,
+        );
+        req2.flush(mockTopTracks);
+        tick();
+        expect(req2.request.method).toBe("GET");
+
+        expect(topItemsStateService.topTracks()).toEqual([mockTopTrackLimited]);
+        expect(topItemsHTTPService.getTopItems$).toHaveBeenCalledTimes(2);
+    }));
+
+    it("should not fetch data again if data for the specified time range has already been loaded", fakeAsync(() => {
+        const timeRange = TimeRangeOptions.LongTerm;
+        topItemsHTTPService.getTopItems$ = jest
+            .fn()
+            .mockReturnValue(of(mockTopTracks));
+
+        topItemsStateService.publishTopTracksTimeRange(timeRange);
+        tick();
+        topItemsStateService.publishTopTracksTimeRange(timeRange);
+        tick();
+
+        expect(topItemsStateService.topTracks()).toEqual([mockTopTrackLimited]);
+        expect(topItemsHTTPService.getTopItems$).toHaveBeenCalledTimes(1);
+    }));
+});
---
+++ src/app/state/top-items-state.service.ts
@@ -0,0 +1,110 @@
+import { computed, inject, Injectable, signal } from "@angular/core";
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
+import { TopItemsHTTPService } from "@api/top-items-http.service";
+import {
+    Album,
+    TimeRangeOptions,
+    TopArtist,
+    TopItemsState,
+    TopTrack,
+    TopTrackLimited,
+} from "@model/top-items.model";
+import { map, of, Subject, switchMap } from "rxjs";
+
+import { TopItemsService } from "../components/routes/top-items.service";
+
+@Injectable({
+    providedIn: "root",
+})
+export class TopItemsStateService {
+    readonly #topItemsHTTPService = inject(TopItemsHTTPService);
+    readonly #topItemsService = inject(TopItemsService);
+
+    readonly #topTracksState = signal<TopItemsState<TopTrackLimited>>({
+        itemsByTimeRange: {
+            [TimeRangeOptions.ShortTerm]: [],
+            [TimeRangeOptions.MediumTerm]: [],
+            [TimeRangeOptions.LongTerm]: [],
+        },
+        currentTimeRange: TimeRangeOptions.ShortTerm,
+    });
+
+    // #region selectors
+    topTracks = computed(
+        () =>
+            this.#topTracksState().itemsByTimeRange[
+                this.#topTracksState().currentTimeRange
+            ],
+    );
+
+    topTracksTimeRange = computed(
+        () => this.#topTracksState().currentTimeRange,
+    );
+    // #endregion
+
+    // #region sources
+    readonly #topTracksTimeRange$ = new Subject<TimeRangeOptions>();
+
+    readonly #topTracks$ = this.#topTracksTimeRange$.pipe(
+        switchMap((timeRange) => {
+            const topTracksByTimeRange =
+                this.#topTracksState().itemsByTimeRange[timeRange];
+            if (topTracksByTimeRange.length) {
+                return of(topTracksByTimeRange);
+            }
+            return this.#topItemsHTTPService
+                .getTopItems$<TopTrack<Album, TopArtist>, "tracks">("tracks", {
+                    time_range: timeRange,
+                    limit: 50,
+                    offset: 0,
+                })
+                .pipe(
+                    map(({ items }) =>
+                        items.map((item) =>
+                            this.#topItemsService.convertTopTrackToLimited(
+                                item,
+                            ),
+                        ),
+                    ),
+                );
+        }),
+    );
+    // #endregion
+
+    constructor() {
+        // #region reducers
+        this.#topTracksTimeRange$
+            .pipe(takeUntilDestroyed())
+            .subscribe((timeRange) => {
+                this.#topTracksState.update(
+                    (state): TopItemsState<TopTrackLimited> => ({
+                        ...state,
+                        currentTimeRange: timeRange,
+                    }),
+                );
+            });
+
+        this.#topTracks$.pipe(takeUntilDestroyed()).subscribe((tracks) =>
+            this.#topTracksState.update(
+                (state): TopItemsState<TopTrackLimited> => ({
+                    ...state,
+                    itemsByTimeRange: {
+                        ...state.itemsByTimeRange,
+                        [state.currentTimeRange]: tracks,
+                    },
+                }),
+            ),
+        );
+        // #endregion
+    }
+
+    /**
+     * Publishes a new time range to the `topTracksTimeRange$` subject, updating the current time
+     * range for top tracks.
+     *
+     * @param timeRange - The new time range to be published.
+     */
+    publishTopTracksTimeRange(timeRange: TimeRangeOptions): void {
+        this.#topTracksTimeRange$.next(timeRange);
+    }
+}
