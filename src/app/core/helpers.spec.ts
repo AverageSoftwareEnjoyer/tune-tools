ef902bd891b5f6021412e1ecf41d89e45f9d7f39
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2024-06-29 15:31:34 +0200

    TT-55: Add tests for timeRangeGuard

---
+++ src/app/core/helpers.spec.ts
@@ -0,0 +1,52 @@
+import { TestBed } from "@angular/core/testing";
+import {
+    ActivatedRouteSnapshot,
+    CanActivateFn,
+    Router,
+    RouterStateSnapshot,
+} from "@angular/router";
+import { TimeRangeOptions, TopItemsRoutes } from "@model/top-items.model";
+
+import { timeRangeGuard } from "./helpers";
+
+describe("timeRangeGuard", () => {
+    let router: Router;
+    let guard: CanActivateFn;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+
+        guard = timeRangeGuard(TopItemsRoutes.TopTracks);
+        router = TestBed.inject(Router);
+    });
+
+    it("should allow navigation if timeRange is valid", () => {
+        const route = {
+            params: { timeRange: TimeRangeOptions.LongTerm },
+        };
+        const result = TestBed.runInInjectionContext(() =>
+            guard(
+                route as unknown as ActivatedRouteSnapshot,
+                {} as unknown as RouterStateSnapshot,
+            ),
+        );
+
+        expect(result).toBe(true);
+    });
+
+    it("should redirect to default route if timeRange is invalid", () => {
+        const route = { params: { timeRange: "InvalidTimeRange" } };
+        const result = TestBed.runInInjectionContext(() =>
+            guard(
+                route as unknown as ActivatedRouteSnapshot,
+                {} as unknown as RouterStateSnapshot,
+            ),
+        );
+
+        expect(result).toEqual(
+            router.parseUrl(
+                `${TopItemsRoutes.TopTracks}/${TimeRangeOptions.ShortTerm}`,
+            ),
+        );
+    });
+});
