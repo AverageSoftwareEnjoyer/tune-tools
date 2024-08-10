1f4c481f4518e9fb8ff43e4ec3d1494516eb47aa
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2024-08-10 14:17:59 +0200

    TT-69: Create a base homepage component

--- src/app/components/routes/home/home.component.spec.ts
+++ src/app/components/routes/home/home.component.spec.ts
@@ -1,16 +1,34 @@
+import { provideHttpClient } from "@angular/common/http";
+import { provideHttpClientTesting } from "@angular/common/http/testing";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { ActivatedRoute } from "@angular/router";
+import { AuthService } from "@core/auth/auth.service";
 
 import { HomeComponent } from "./home.component";
 
 describe("HomeComponent", () => {
     let component: HomeComponent;
     let fixture: ComponentFixture<HomeComponent>;
+    let authService: AuthService;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            imports: [HomeComponent],
+            imports: [HomeComponent, NoopAnimationsModule],
+            providers: [
+                {
+                    provide: ActivatedRoute,
+                    useValue: {
+                        snapshot: {},
+                    },
+                },
+                provideHttpClient(),
+                provideHttpClientTesting(),
+            ],
         });
 
+        authService = TestBed.inject(AuthService);
         fixture = TestBed.createComponent(HomeComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
@@ -18,5 +36,17 @@ describe("HomeComponent", () => {
 
     it("should create", () => {
         expect(component).toBeTruthy();
+    });
+
+    it("should redirect the user to Spotify auth flow on button click if they are not authenticated", () => {
+        const redirectToAuthCodeFlowSpy = jest.spyOn(
+            authService,
+            "redirectToAuthCodeFlow$",
+        );
+        const button = fixture.debugElement.query(By.css("#cta-1"));
+
+        button.triggerEventHandler("click", null);
+
+        expect(redirectToAuthCodeFlowSpy).toHaveBeenCalledTimes(1);
     });
 });
