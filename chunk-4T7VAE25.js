3e95dfedc91124eb15acfefd80c7bfa13eaa2a6b
Domin8668 <github-actions[bot] 41898282+github-actions[bot]@users.noreply.github.com>
2024-09-23 17:15:19 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/AverageSoftwareEnjoyer/tune-tools/commit/c8ad6aa6a832e0889d7637863024502d430527a0

--- chunk-4T7VAE25.js
+++
@@ -1,2 +0,0 @@
-import{Jb as a,Kb as r,ac as i,hc as p,na as o}from"./chunk-RPA4O24S.js";var c=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=o({type:e,selectors:[["app-recently-played"]],standalone:!0,features:[p],decls:2,vars:0,template:function(n,d){n&1&&(a(0,"p"),i(1,"recently-played works!"),r())},changeDetection:0});let t=e;return t})();var y=[{path:":view-type",component:c},{path:"**",redirectTo:"unsorted"}];export{y as default};
-//# sourceMappingURL=chunk-4T7VAE25.js.map
